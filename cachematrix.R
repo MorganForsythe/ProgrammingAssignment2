1b9a12680b32c47c3700780e45a434660c2a852d
MorganForsythe <mforsythe77@hotmail.com>
2014-05-25 08:51:25 -0400

    These are my commit notes.

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,7 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+this is a test of changes
 }
 
 
@@ -13,3 +13,5 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
+
+https://class.coursera.org/rprog-003/forum/thread?thread_id=1089
